aa4b3674a0194ed8dfe04162ff0fb46b17768af2
GiKode <827266641@qq.com>
2020-03-01 11:03:26 +0800

    1. Update  action
    Signed-off-by: Gikode <827266641@qq.com>

--- .github/scripts/ds.sh
+++ .github/scripts/ds.sh
@@ -2,11 +2,11 @@
 
 # Decrypt the file
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $GITHUB_WORKSPACE/.github/secrets/keystore.properties $GITHUB_WORKSPACE/.github/secrets/keystore.properties.gpg
+--output $GITHUB_WORKSPACE/.github/workflows/keystore.properties $GITHUB_WORKSPACE/.github/secrets/keystore.properties.gpg
 
 echo "Decrypt key.properties done"
 
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $GITHUB_WORKSPACE/.github/secrets/key.jks $GITHUB_WORKSPACE/.github/secrets/key.jks.gpg
+--output $GITHUB_WORKSPACE/.github/workflows/key.jks $GITHUB_WORKSPACE/.github/secrets/key.jks.gpg
 
 echo "Decrypt key.jks done"
