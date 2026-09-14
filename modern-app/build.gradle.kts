88f014c801f5d1bcba8b3e676da7914a1dbb9ee6
GiKode <827266641@qq.com>
2026-09-14 16:36:08 +0800

    build: bump Compose rewrite to alpha05

--- modern-app/build.gradle.kts
+++ modern-app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "top.geek_studio.chenlongcould.musicplayer"
         minSdk = 23
         targetSdk = 37
-        versionCode = 203
-        versionName = "2.0.0-alpha04"
+        versionCode = 204
+        versionName = "2.0.0-alpha05"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
