4e979d3ac49e452026482411116bffb923444243
GiKode <827266641@qq.com>
2026-09-14 21:45:10 +0800

    fix: compile managed-device Compose tests

--- modern-app/src/androidTest/kotlin/top/geek_studio/chenlongcould/musicplayer/ui/PlaylistImportPreviewInstrumentedTest.kt
+++ modern-app/src/androidTest/kotlin/top/geek_studio/chenlongcould/musicplayer/ui/PlaylistImportPreviewInstrumentedTest.kt
@@ -1,7 +1,6 @@
 package top.geek_studio.chenlongcould.musicplayer.ui
 
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.ui.test.assertDoesNotExist
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
