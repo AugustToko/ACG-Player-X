70173040b7e24c7841fe95dc89c32e001363ab71
GiKode <827266641@qq.com>
2026-09-14 14:57:32 +0800

    fix: use scoped Compose weight extension

--- modern-app/src/main/kotlin/top/geek_studio/chenlongcould/musicplayer/ui/AcgPlayerApp.kt
+++ modern-app/src/main/kotlin/top/geek_studio/chenlongcould/musicplayer/ui/AcgPlayerApp.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
